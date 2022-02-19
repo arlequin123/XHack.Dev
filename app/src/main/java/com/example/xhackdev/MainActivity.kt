d9e1ff4b1805c0e1f5936fdb7df945048f3473e1
arlequin123 <36443483+arlequin123@users.noreply.github.com>
2022-02-19 21:12:43 +0300

    init splash screen

--- app/src/main/java/com/example/xhackdev/MainActivity.kt
+++ app/src/main/java/com/example/xhackdev/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.xhackdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewTreeObserver
+import android.widget.TextView
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        val content = findViewById<TextView>(R.id.textview)
+
+        content.viewTreeObserver.addOnPreDrawListener (
+            object: ViewTreeObserver.OnPreDrawListener{
+                override fun onPreDraw(): Boolean {
+                    if (false){
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+            }
+        )
     }
 }
